21ef1a92fe394dacc86e60e8832ed1d2712de01d
eriasu <eliasxx@gmail.com>
2013-03-01 15:53:46 -0300

    inicio de partes base del juego

--- Main/AeroFigtherJE.py
+++ Main/AeroFigtherJE.py
@@ -1,12 +1,17 @@
 __author__ = 'eriasu'
 #Imports
+import pygame
+from pygame.locals import  *
 #-------
 #Constantes
+Largo   =   600
+Ancho   =   800
 #---------
 #Clases
 #
 #Funciones
 def main():
+    pygame.init()
     return 0
 #
 #Funcion MAin
